5232aa24b3cbeb58b333bb8619947fd3b5fa08d3
Nikki Little <nikkilittle13@gmail.com>
2024-01-23 14:08:43 -0500

    converted temperature from celsius to fahrenheit

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-/*fetch("https://www.7timer.info/bin/civillight.php?lon=-75.16&lat=39.95&ac=0&unit=metric&output=json&tzshift=0")
-  .then(resp => resp.json())
-  .then(data => {
-    const h2 = document.createElement("h2");
-    const dateFromAPI = data.dataseries[0].date;
-    const date = new Date(dateFromAPI.toString().substr(0, 4), dateFromAPI.toString().substr(4, 2) - 1, dateFromAPI.toString().substr(6, 2));
-
-    const options = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      timeZone: 'UTC'
-    };
-
-    const formattedDate = date.toLocaleDateString('en-US', options);
-    h2.textContent = formattedDate;
-    document.querySelector(".day-of-week").appendChild(h2);
-  })
-  .catch(error => console.log(error));
-  */
-
-  fetch("https://www.7timer.info/bin/civillight.php?lon=-75.16&lat=39.95&ac=0&unit=metric&output=json&tzshift=0")
+fetch("https://www.7timer.info/bin/civillight.php?lon=-75.16&lat=39.95&ac=0&unit=metric&output=json&tzshift=0")
 .then(resp => resp.json())
 .then(data => {
 const jsonContainer = document.getElementById('jsonContainer');
@@ -41,15 +19,18 @@ dataseries.forEach(obj => {
   const formattedDate = date.toLocaleDateString('en-US', options);
 
   const jsonDate = document.createElement('h2');
-  const jsonItem = document.createElement('div');
- // const jsonItem = document.createElement('div');
- // const jsonItem = document.createElement('div');
+  const weatherIcon = document.createElement('div');
 
   jsonDate.textContent = `${formattedDate}`;
 
-  jsonItem.textContent = `Weather: ${obj.weather}, Max Temp: ${obj.temp2m.max}, Min Temp: ${obj.temp2m.min}, Max Wind Speed: ${obj.wind10m_max}`;
+  const maxTempCelsius = obj.temp2m.max;
+  const minTempCelsius = obj.temp2m.min;
+  const maxTempFahrenheit = (maxTempCelsius * 9/5) + 32;
+  const minTempFahrenheit = (minTempCelsius * 9/5) + 32;
+
+  weatherIcon.textContent = `Weather: ${obj.weather}, Max Temp: ${maxTempFahrenheit}, Min Temp: ${minTempFahrenheit}, Max Wind Speed: ${obj.wind10m_max}`;
   jsonContainer.appendChild(jsonDate);
-  jsonContainer.appendChild(jsonItem);
+  jsonContainer.appendChild(weatherIcon);
 });
 })
 .catch(error => console.log(error));
